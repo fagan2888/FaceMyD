20f52bddebaf468f095779091c491f10c1b658a3
= <gudbrandduff@gmail.com>
2017-11-05 16:41:28 -0800

    capture new user added

---
+++ capture_new_user.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+data_dir = "./Data/"
+
+def capture_new_user(name):
+	N = 10
+	
+	for i in range(N):
+		outfile = data_dir + name + str(i) + ".bmp"
+#		#res = os.system("raspistill -o " + outfile)
+		print("captured image " + outfile)
+
+if __name__ == "__main__":
+	name = sys.argv[1]
+	capture_new_user(name)
+
+
